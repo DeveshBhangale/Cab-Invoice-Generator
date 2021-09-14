0ccbc984985742036865c2cb07158b680b76e7e8
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-09-14 22:55:35 +0530

    [Devesh]: added feature to calculate fare

--- Cab-Invoice-Generator/lib/src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoice.java
+++ Cab-Invoice-Generator/lib/src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoice.java
@@ -1,5 +1,15 @@
 package com.bridgelabz.cabinvoicegenerator;
 
 public class CabInvoice {
-
+	private final static int ratePerKilometer = 10;
+	private final static int ratePerMinute = 1;
+	private final static int minimumFare = 5;
+	private double totalFare;
+	
+	public double calculateFare(double totalKm, double totalTime) {
+		totalFare = (double)((totalKm*ratePerKilometer)+(totalTime*ratePerMinute));
+		if((int)totalFare <= minimumFare) return minimumFare;
+		return totalFare;
+	}
+	
 }
--- Cab-Invoice-Generator/lib/src/test/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceTest.java
+++ Cab-Invoice-Generator/lib/src/test/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceTest.java
@@ -1,4 +1,14 @@
 package com.bridgelabz.cabinvoicegenerator;
 
+import org.junit.Test;
+
+import org.junit.Assert;
+
 public class CabInvoiceTest {
+	
+	@Test
+	public void shouldReturnTotalFare() {
+		CabInvoice cabInvoice = new CabInvoice();
+		Assert.assertEquals(33.0, cabInvoice.calculateFare(1.8,15),0.0);
+	}
 }

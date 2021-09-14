e6be7a8532146891524dcd662c45ef045e1f5e92
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-09-14 23:20:50 +0530

    [Devesh]: added feature to calculate fare for mulitple rides

--- Cab-Invoice-Generator/lib/src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoice.java
+++ Cab-Invoice-Generator/lib/src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoice.java
@@ -1,15 +1,32 @@
 package com.bridgelabz.cabinvoicegenerator;
 
+import java.util.ArrayList;
+
 public class CabInvoice {
 	private final static int ratePerKilometer = 10;
 	private final static int ratePerMinute = 1;
 	private final static int minimumFare = 5;
-	private double totalFare;
+	private double totalFare,totalKm,totaltime;
 	
-	public double calculateFare(double totalKm, double totalTime) {
-		totalFare = (double)((totalKm*ratePerKilometer)+(totalTime*ratePerMinute));
+	public CabInvoice() {}
+	
+	public CabInvoice(double totalKm, double totaltime) {
+		super();
+		this.totalKm = totalKm;
+		this.totaltime = totaltime;
+	}
+
+	
+	
+	
+	public double calculateFare() {
+		totalFare = (double)((this.totalKm*ratePerKilometer)+(this.totaltime*ratePerMinute));
 		if((int)totalFare <= minimumFare) return minimumFare;
 		return totalFare;
+	}
+	
+	public void calculateFareForMultipleRides(ArrayList<CabInvoice> l1) {
+		l1.stream().forEach(n -> System.out.println(n.calculateFare()));
 	}
 	
 }
--- Cab-Invoice-Generator/lib/src/test/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceTest.java
+++ Cab-Invoice-Generator/lib/src/test/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceTest.java
@@ -2,13 +2,29 @@ package com.bridgelabz.cabinvoicegenerator;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import org.junit.Assert;
 
 public class CabInvoiceTest {
 	
+	
+	ArrayList<CabInvoice> l1 = new ArrayList<>();
+	
+	
 	@Test
 	public void shouldReturnTotalFare() {
-		CabInvoice cabInvoice = new CabInvoice();
-		Assert.assertEquals(33.0, cabInvoice.calculateFare(1.8,15),0.0);
+		CabInvoice cabInvoice = new CabInvoice(1.8,15);
+		Assert.assertEquals(33.0, cabInvoice.calculateFare(),0.0);
 	}
+	
+	@Test
+	public void shouldReturnTotalFareForMultipleRides() {
+		l1.add(new CabInvoice(1.8,15));
+		l1.add(new CabInvoice(2,16));
+		l1.add(new CabInvoice(4,30));
+		CabInvoice cabInvoice = new CabInvoice();
+		cabInvoice.calculateFareForMultipleRides(l1);
+	}
+	
 }
